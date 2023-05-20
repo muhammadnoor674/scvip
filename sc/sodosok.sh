183bc8ad92fc779e3fff170b761dab9a09f2ca6b
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-20 22:21:23 +0800

    Update sodosok.sh

--- sc/sodosok.sh
+++ sc/sodosok.sh
@@ -4,10 +4,10 @@ sleep 0.5
 echo "Memulai Install SS OBFS"
 sleep 1
 
-wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/CDN/A/I/U/E/O/sodosoko.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/CDN/A/I/U/E/O/sodosoko.sh
 chmod +x sodosoko.sh
 ./sodosoko.sh
 rm -f sodosoko.sh
 
-echo -e "Powered By Ridwan"
+echo -e "Powered By NYARIGRATISAN"
 rm -f sodosok.sh
