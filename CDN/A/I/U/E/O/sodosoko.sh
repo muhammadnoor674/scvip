be5c6e7ee480ee3c8e4c80cd2bc208611fb80941
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-20 21:21:35 +0800

    Update sodosoko.sh

--- CDN/A/I/U/E/O/sodosoko.sh
+++ CDN/A/I/U/E/O/sodosoko.sh
@@ -88,6 +88,6 @@ iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 ip6tables-save > /etc/ip6tables.up.rules
 echo -e "Done Install Shadowsocks-libev" | lolcat
-figlet -f slant RIDWAN - STORE | lolcat
+figlet -f slant NYARIGRATISAN - STORE | lolcat
 sleep 1
 rm -f /root/sodosoko.sh
