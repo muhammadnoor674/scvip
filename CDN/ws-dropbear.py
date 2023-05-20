80704a25a80d61f85ef486a2556d41584d791554
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-20 21:10:54 +0800

    Update ws-dropbear.py

--- CDN/ws-dropbear.py
+++ CDN/ws-dropbear.py
@@ -15,7 +15,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:143'
-RESPONSE = 'HTTP/1.1 101 <b><font color="#5f4dee">DRG TEAM</font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <b><font color="#5f4dee">NYARIGRATISAN</font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
