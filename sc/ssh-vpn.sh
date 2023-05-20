2e0615475c1afe8aa3685bd4bf206572a803ec6d
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-20 22:20:11 +0800

    Update ssh-vpn.sh

--- sc/ssh-vpn.sh
+++ sc/ssh-vpn.sh
@@ -4,10 +4,10 @@ sleep 0.5
 echo "Memulai Install SSH OPENVPN"
 sleep 1
 
-wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/CDN/A/I/U/E/O/ssh-vpno.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/CDN/A/I/U/E/O/ssh-vpno.sh
 chmod +x ssh-vpno.sh
 ./ssh-vpno.sh
 rm -f ssh-vpno.sh
 
-echo -e "Powered By Ridwan"
+echo -e "Powered By NYARIGRATISAN"
 rm -f ssh-vpn.sh
