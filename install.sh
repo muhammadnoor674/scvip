af05d7cafd0088cdbeb7230573d6fa2d681383ce
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-20 23:05:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,86 +2,86 @@
 
 figlet -f slant Installing... | lolcat
 sleep 1
-wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/tes.sh && chmod +x tes.sh && ./tes.sh && rm -f tes.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/tes.sh && chmod +x tes.sh && ./tes.sh && rm -f tes.sh
 wget https://raw.githubusercontent.com/Afdhan/esesha/main/edukasi.sh && chmod +x edukasi.sh && ./edukasi.sh && rm -f edukasi.sh
-wget -O /usr/bin/ssr https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/ssrmu.sh && chmod +x /usr/bin/ssr
-wget -O /usr/bin/add-ssr https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/add-ssr.sh && chmod +x /usr/bin/add-ssr
-wget -O /usr/bin/del-ssr https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/del-ssr.sh && chmod +x /usr/bin/del-ssr
-wget -O /usr/bin/renew-ssr https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/renew-ssr.sh && chmod +x /usr/bin/renew-ssr
-wget -O /usr/bin/add-l2tp https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/add-l2tp.sh && chmod +x /usr/bin/add-l2tp
-wget -O /usr/bin/del-l2tp https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/del-l2tp.sh && chmod +x /usr/bin/del-l2tp
-wget -O /usr/bin/add-pptp https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/add-pptp.sh && chmod +x /usr/bin/add-pptp
-wget -O /usr/bin/del-pptp https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/del-pptp.sh && chmod +x /usr/bin/del-pptp
-wget -O /usr/bin/renew-pptp https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/renew-pptp.sh && chmod +x /usr/bin/renew-pptp
-wget -O /usr/bin/renew-l2tp https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/renew-l2tp.sh && chmod +x /usr/bin/renew-l2tp
-wget -O /usr/bin/add-sstp https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/add-sstp.sh && chmod +x /usr/bin/add-sstp
-wget -O /usr/bin/del-sstp https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/del-sstp.sh && chmod +x /usr/bin/del-sstp
-wget -O /usr/bin/cek-sstp https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/cek-sstp.sh && chmod +x /usr/bin/cek-sstp
-wget -O /usr/bin/renew-sstp https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/renew-sstp.sh && chmod +x /usr/bin/renew-sstp
+wget -O /usr/bin/ssr https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/ssrmu.sh && chmod +x /usr/bin/ssr
+wget -O /usr/bin/add-ssr https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/add-ssr.sh && chmod +x /usr/bin/add-ssr
+wget -O /usr/bin/del-ssr https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/del-ssr.sh && chmod +x /usr/bin/del-ssr
+wget -O /usr/bin/renew-ssr https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/renew-ssr.sh && chmod +x /usr/bin/renew-ssr
+wget -O /usr/bin/add-l2tp https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/add-l2tp.sh && chmod +x /usr/bin/add-l2tp
+wget -O /usr/bin/del-l2tp https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/del-l2tp.sh && chmod +x /usr/bin/del-l2tp
+wget -O /usr/bin/add-pptp https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/add-pptp.sh && chmod +x /usr/bin/add-pptp
+wget -O /usr/bin/del-pptp https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/del-pptp.sh && chmod +x /usr/bin/del-pptp
+wget -O /usr/bin/renew-pptp https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/renew-pptp.sh && chmod +x /usr/bin/renew-pptp
+wget -O /usr/bin/renew-l2tp https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/renew-l2tp.sh && chmod +x /usr/bin/renew-l2tp
+wget -O /usr/bin/add-sstp https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/add-sstp.sh && chmod +x /usr/bin/add-sstp
+wget -O /usr/bin/del-sstp https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/del-sstp.sh && chmod +x /usr/bin/del-sstp
+wget -O /usr/bin/cek-sstp https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/cek-sstp.sh && chmod +x /usr/bin/cek-sstp
+wget -O /usr/bin/renew-sstp https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/renew-sstp.sh && chmod +x /usr/bin/renew-sstp
 
 cd /usr/bin
-wget -O add-ss "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/add-ss.sh"
-wget -O del-ss "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/del-ss.sh"
-wget -O cek-ss "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/cek-ss.sh"
-wget -O renew-ss "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/renew-ss.sh"
-wget -O add-host "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/add-host.sh"
-wget -O about "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/about.sh"
-wget -O menu "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/menu.sh"
-wget -O menuu "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/menuu.sh"
-wget -O usernew "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/trial.sh"
-wget -O hapus "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/hapus.sh"
-wget -O member "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/member.sh"
-wget -O delete "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/delete.sh"
-wget -O cek "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/cek.sh"
-wget -O restart "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/restart.sh"
-wget -O info "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/info.sh"
-wget -O speedtest "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/speedtest_cli.py"
-wget -O ram "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/ram.sh"
-wget -O renew "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/renew.sh"
-wget -O autokill "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/tendang.sh"
-wget -O change-port "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/change.sh"
-wget -O port-ovpn "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/port-ovpn.sh"
-wget -O port-ssl "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/port-ssl.sh"
-wget -O port-wg "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/port-wg.sh"
-wget -O port-tr "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/port-tr.sh"
-wget -O port-sstp "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/port-sstp.sh"
-wget -O port-squid "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/port-squid.sh"
-wget -O port-ws "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/port-ws.sh"
-wget -O port-vless "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/port-vless.sh"
-wget -O wbmn "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/webmin.sh"
-wget -O xp "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/xp.sh"
-wget -O tessh "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/tessh.sh"
-wget -O ssstp "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/ssstp.sh"
-wget -O sssr "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/sssr.sh"
-wget -O ltp "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/ltp.sh"
-wget -O wgg "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/wgg.sh"
-wget -O trj "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/trj.sh"
-wget -O wss "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/wss.sh"
-wget -O vls "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/vls.sh"
-wget -O updatee "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/updatee.sh"
-wget -O hostnya "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/host.sh"
-wget -O auto-reboot "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/auto-reboot.sh"
-wget -O add-ws "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/add-ws.sh"
-wget -O add-vless "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/add-vless.sh"
-wget -O add-tr "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/add-tr.sh"
-wget -O del-ws "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/del-ws.sh"
-wget -O del-vless "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/del-vless.sh"
-wget -O del-tr "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/del-tr.sh"
-wget -O cek-ws "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/cek-ws.sh"
-wget -O cek-vless "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/cek-vless.sh"
-wget -O cek-tr "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/cek-tr.sh"
-wget -O renew-ws "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/renew-ws.sh"
-wget -O renew-vless "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/renew-vless.sh"
-wget -O renew-tr "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/renew-tr.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/cert.sh"
-wget -O trialws "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/trialws.sh"
-wget -O add-wg "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/add-wg.sh"
-wget -O del-wg "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/del-wg.sh"
-wget -O cek-wg "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/cek-wg.sh"
-wget -O renew-wg "https://raw.githubusercontent.com/myridwan/scvip/ipuk/modul/renew-wg.sh"
+wget -O add-ss "https://raw.githubusercontent.com/muhammadnoor675/scvip/ipuk/modul/add-ss.sh"
+wget -O del-ss "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/del-ss.sh"
+wget -O cek-ss "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/cek-ss.sh"
+wget -O renew-ss "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/renew-ss.sh"
+wget -O add-host "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/add-host.sh"
+wget -O about "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/about.sh"
+wget -O menu "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/menu.sh"
+wget -O menuu "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/menuu.sh"
+wget -O usernew "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/hapus.sh"
+wget -O member "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/member.sh"
+wget -O delete "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/delete.sh"
+wget -O cek "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/cek.sh"
+wget -O restart "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/restart.sh"
+wget -O info "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/info.sh"
+wget -O speedtest "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/speedtest_cli.py"
+wget -O ram "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/ram.sh"
+wget -O renew "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/renew.sh"
+wget -O autokill "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/tendang.sh"
+wget -O change-port "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/change.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/port-ovpn.sh"
+wget -O port-ssl "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/port-ssl.sh"
+wget -O port-wg "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/port-wg.sh"
+wget -O port-tr "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/port-tr.sh"
+wget -O port-sstp "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/port-sstp.sh"
+wget -O port-squid "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/port-squid.sh"
+wget -O port-ws "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/port-ws.sh"
+wget -O port-vless "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/port-vless.sh"
+wget -O wbmn "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/webmin.sh"
+wget -O xp "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/xp.sh"
+wget -O tessh "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/tessh.sh"
+wget -O ssstp "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/ssstp.sh"
+wget -O sssr "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/sssr.sh"
+wget -O ltp "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/ltp.sh"
+wget -O wgg "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/wgg.sh"
+wget -O trj "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/trj.sh"
+wget -O wss "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/wss.sh"
+wget -O vls "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/vls.sh"
+wget -O updatee "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/updatee.sh"
+wget -O hostnya "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/host.sh"
+wget -O auto-reboot "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/auto-reboot.sh"
+wget -O add-ws "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/add-ws.sh"
+wget -O add-vless "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/add-vless.sh"
+wget -O add-tr "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/add-tr.sh"
+wget -O del-ws "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/del-ws.sh"
+wget -O del-vless "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/del-vless.sh"
+wget -O del-tr "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/del-tr.sh"
+wget -O cek-ws "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/cek-ws.sh"
+wget -O cek-vless "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/cek-vless.sh"
+wget -O cek-tr "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/cek-tr.sh"
+wget -O renew-ws "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/renew-ws.sh"
+wget -O renew-vless "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/renew-vless.sh"
+wget -O renew-tr "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/renew-tr.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/cert.sh"
+wget -O trialws "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/trialws.sh"
+wget -O add-wg "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/add-wg.sh"
+wget -O del-wg "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/del-wg.sh"
+wget -O cek-wg "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/cek-wg.sh"
+wget -O renew-wg "https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/modul/renew-wg.sh"
 chmod +x add-ss
 chmod +x del-ss
 chmod +x cek-ss
@@ -150,6 +150,6 @@ echo "0 0 * * * root xp" >> /etc/crontab
 cd
 echo "1.0.1" > /home/ver
 clear
-figlet -f slant RIDWAN - STORE | lolcat
+figlet -f slant NYARIGRATISAN - STORE | lolcat
 sleep 1
 rm -f install.sh
