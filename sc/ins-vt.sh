a46da44ad240f1824478d7e9abb4408d7b98e28a
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-20 22:25:48 +0800

    Update ins-vt.sh

--- sc/ins-vt.sh
+++ sc/ins-vt.sh
@@ -4,10 +4,10 @@ sleep 0.5
 echo "Memulai Install V2RAY"
 sleep 1
 
-wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/CDN/A/I/U/E/O/ins-vto.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/CDN/A/I/U/E/O/ins-vto.sh
 chmod +x ins-vto.sh
 ./ins-vto.sh
 rm -f ins-vto.sh
 
-echo -e "Powered By Ridwan"
+echo -e "Powered By NYARIGRATISAN"
 rm -f ins-vt.sh
