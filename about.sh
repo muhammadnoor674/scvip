2b2646ce9fee8b887f028987a3e4fc7e4f673203
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-20 22:41:38 +0800

    Update about.sh

--- about.sh
+++ about.sh
@@ -2,14 +2,14 @@
 
 clear
 echo -e "=================================================" | lolcat
-echo -e "#     AutoScript VPS By M RIDWAN - STORE      #"
+echo -e "#     AutoScript VPS By NYARIGRATISAN - STORE      #"
 echo -e "#-----------------------------------------------#"
 echo -e "#             OPERATING SYSTEM                  #"
 echo -e "             -------------------                 "
 echo -e "# For Debian 9 & Debian 10 64 bit               #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
-echo -e "# Build Up By M RIDWAN - STORE              #"
+echo -e "# Build Up By NYARIGRATISAN - STORE              #"
 echo -e "#-----------------------------------------------#"
 echo -e "# Terima Kasih Kepada :                         #"
 echo -e "#-----------------------------------------------#"
@@ -20,5 +20,5 @@ echo -e "# Makhluk Bumi                                  #"
 echo -e "# Icipers Nusantara                             #"
 echo -e "# All Admin Dan All Member                      #"
 echo -e "#-----------------------------------------------#"
-echo -e "- Mod By M RIDWAN - STORE"
+echo -e "- Mod By M NYARIGRATISAN - STORE"
 echo -e "=================================================" | lolcat
