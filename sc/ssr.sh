a7c3083c31107616a46fe8945a2e035d23888632
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-20 22:19:10 +0800

    Update ssr.sh

--- sc/ssr.sh
+++ sc/ssr.sh
@@ -4,10 +4,10 @@ sleep 0.5
 echo "Memulai Install SSR"
 sleep 1
 
-wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/CDN/A/I/U/E/O/ssro.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/CDN/A/I/U/E/O/ssro.sh
 chmod +x ssro.sh
 ./ssro.sh
 rm -f ssro.sh
 
-echo -e "Powered By Ridwan"
+echo -e "Powered By NYARIGRATISAN"
 rm -f ssr.sh
