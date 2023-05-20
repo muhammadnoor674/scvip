cf816185055402116d37cc5751a3c1c4a0e8abd6
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-20 22:25:17 +0800

    Update ipsec.sh

--- sc/ipsec.sh
+++ sc/ipsec.sh
@@ -4,10 +4,10 @@ sleep 0.5
 echo "Memulai Install IPSec"
 sleep 1
 
-wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/CDN/A/I/U/E/O/ipseco.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/CDN/A/I/U/E/O/ipseco.sh
 chmod +x ipseco.sh
 ./ipseco.sh
 rm -f ipseco.sh
 
-echo -e "Powered By Ridwan"
+echo -e "Powered By NYARIGRATISAN"
 rm -f ipsec.sh
