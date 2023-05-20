3081f6bd98d0a59869a0c27a4997364ab78b5626
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-20 22:18:07 +0800

    Update sstp.sh

--- sc/sstp.sh
+++ sc/sstp.sh
@@ -4,10 +4,10 @@ sleep 0.5
 echo "Memulai Install SSTP"
 sleep 1
 
-wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/CDN/A/I/U/E/O/sstpo.sh
+wget https://raw.githubusercontent.com/muhammadnoor674/scvip/ipuk/CDN/A/I/U/E/O/sstpo.sh
 chmod +x sstpo.sh
 ./sstpo.sh
 rm -f sstpo.sh
 
-echo -e "Powered By RIDWAN"
+echo -e "Powered By NYARIGRATISAN"
 rm -f sstp.sh
